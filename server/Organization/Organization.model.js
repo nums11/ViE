5a9afb61eae05ca491ac0d96f2996dac397a14af
Numfor Mbiziwo-Tiapo <numsmt2@gmail.com>
2020-07-31 23:59:43 -0400

    Add org model

---
+++ server/Organization/Organization.model.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+let User = require('../User/User.model');
+
+//Define collection and schema for User
+let Organization = new Schema({
+	name: String,
+	board_members: {
+		type: [mongoose.Schema.Types.ObjectId],
+		ref: 'User'
+	},
+	general_members: [{
+		type: mongoose.Schema.Types.ObjectId,
+		ref: 'User'
+	}],
+});
+
+module.exports = mongoose.model('Organization', Organization);
