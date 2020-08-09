11980689b8a3a556b8c30c9e35f7d9545b6288a9
Numfor Mbiziwo-Tiapo <numsmt2@gmail.com>
2020-08-09 12:17:46 -0400

    Add course and org to meeting

--- server/Meeting/Meeting.model.js
+++ server/Meeting/Meeting.model.js
@@ -3,12 +3,23 @@ const Schema = mongoose.Schema;
 
 let LiveAttendance = require('../LiveAttendance/LiveAttendance.model');
 let AsyncAttendance = require('../AsyncAttendance/AsyncAttendance.model');
+let Course = require('../Course/Course.model');
+let Organization = require('../Organization/Organization.model');
 
 //Define collection and schema for User
 let Meeting = new Schema({
 	title: String,
   start_time: Date,
   end_time: Date,
+  for_course: {type: Boolean, default: false},
+  course: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Course'
+  },
+  org: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Organization'
+  },
   has_live_attendance: {type: Boolean, default: false},
   has_async_attendance: {type: Boolean, default: false},
   live_attendance: {
