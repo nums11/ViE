203cc66038cbe2c7361c8f92cd3cd3ca4d0f5b59
Abdul <muiz.rpi@gmail.com>
2020-08-22 18:49:32 -0400

    Added ui for showing attendance list next to qr code

--- src/components/TaskInfoModalInstructorExpanded.vue
+++ src/components/TaskInfoModalInstructorExpanded.vue
@@ -1,75 +1,118 @@
 <template>
 
     <div class="task-info-modal-instructor-expanded">
-        <div class="header-area">
-            <div class="left-side">
-                <div class="title-area"><h4>{{ getTaskTitle () }}</h4></div>
-                <div class="subtitle-area">{{ getTaskDateTime () }}</div>
-            </div>
-            <div class="right-side">
-                <div class="icon-area">
-                    <!-- SPACE AVAILABLE -->
-                    <sui-button 
-                    v-if="taskInfo.taskType == 'qr-code'"
-                    @click="expandQRCode"
-                    compact icon="expand" />
+        <div class="left-content-area">
+            <div class="header-area">
+                <div class="left-side">
+                    <div class="title-area"><h4>{{ getTaskTitle () }}</h4></div>
+                    <div class="subtitle-area">{{ getTaskDateTime () }}</div>
+                </div>
+                <div class="right-side">
+                    <div class="icon-area">
+                        <!-- SPACE AVAILABLE -->
+                        <sui-button 
+                        v-if="taskInfo.taskType == 'qr-code'"
+                        @click="expandQRCode"
+                        compact icon="expand" />
+                    </div>
                 </div>
             </div>
-        </div>
 
-        <!-- Body Area -->
-        <div class="body-area">
-            <div class="body-contents">
-                <qrcode 
-                    :style="{margin: '0 auto'}"
-                    v-if="taskInfo.taskType == 'qr-code'"
-                    :value="getUrlEncoded()"
-                    :options="{
-                        width: 400,
-                    }"
-                />
+            <!-- Body Area -->
+            <div class="body-area">
+                <div class="body-contents">
+                    <qrcode 
+                        :style="{margin: '0 auto'}"
+                        v-if="taskInfo.taskType == 'qr-code'"
+                        :value="getUrlEncoded()"
+                        :options="{
+                            width: 400,
+                        }"
+                    />
+                </div>
             </div>
-        </div>
 
-        <!-- Footer Area -->
-        <div class="footer-area">
-            <div class="left-side">
-                <sui-button 
-                    compact icon="left arrow" 
-                    label-position="left" 
-                    @click="cancelTask"
-                    content="Back" />
+            <!-- Footer Area -->
+            <div class="footer-area">
+                <div class="left-side">
+                    <sui-button 
+                        compact icon="left arrow" 
+                        label-position="left" 
+                        @click="cancelTask"
+                        content="Back" />
+                </div>
+                <div class="center-area">
+                    <ProgressBar :value="0.8" />
+                </div>
+                <div class="right-side">
+                    <!-- RIGHT FOOTER PLACEHOLDER -->
+                </div>
             </div>
-            <div class="center-area">
-                <ProgressBar :value="0.8" />
-            </div>
-            <div class="right-side">
-                <!-- RIGHT FOOTER PLACEHOLDER -->
-            </div>
-        </div>
 
-        <transition name="fade" mode="out-in">
-            <div class="fullscreen-modal" v-if="show_fullscreen_modal">
+            <transition name="fade" mode="out-in">
+                <div class="fullscreen-modal" v-if="show_fullscreen_modal">
 
-                <div class="full-content-area">
+                    <div class="full-content-area">
 
-                    <div class="qr-code-fullscreen" v-if="taskInfo.taskType == 'qr-code'">
-                        <qrcode 
-                            :style="{margin: '0 auto'}"
-                            :value="getUrlEncoded()"
-                            :options="{
-                                width: 800
-                            }"
-                        />
+                        <div class="qr-code-fullscreen" v-if="taskInfo.taskType == 'qr-code'">
+                            <qrcode 
+                                :style="{margin: '0 auto'}"
+                                :value="getUrlEncoded()"
+                                :options="{
+                                    width: 800
+                                }"
+                            />
+                        </div>
+
                     </div>
 
+                    <div class="bottom-controls">
+                        <sui-button @click="show_fullscreen_modal = false">Close</sui-button>
+                    </div>
+                </div>
+            </transition>
+        </div>
+        <hide-at breakpoint="small">
+            <div class="attendance-area" v-if="taskInfo.taskType == 'qr-code'">
+            
+                <div class="header-area">
+                    <div class="status-col">STATUS</div>
+                    <div class="student-col">STUDENT NAME</div>
+                    <div class="total-col">15/30 Attended</div>
                 </div>
 
-                <div class="bottom-controls">
-                    <sui-button @click="show_fullscreen_modal = false">Close</sui-button>
+                <div class="attendance-entry-container">
+                    <div class="entry-area" v-for="i in 50" :key="i">
+                        <div class="status-col">
+
+                            <!-- Lottie Status Animation -->
+                            <v-lottie-player
+                                name="check"
+                                :animationData="require('@/assets/lottie/check.json')"
+                                width="20px"
+                                height="20px"
+                                loop="1"
+                                :style="{margin: '0 auto'}"
+                            />
+
+                        </div>
+                        <div class="student-col">Firstname Lastname</div>
+                    </div>
                 </div>
+
+                <div class="footer-area">
+                    <div class="spacer"></div>
+                    <div class="button-area">
+                        <sui-button 
+                            compact icon="chart line" 
+                            label-position="left"
+                            class="venue-blue"
+                            content="Stats" />
+                    </div>
+                </div>
+
             </div>
-        </transition>
+        </hide-at>
     </div>
 
 </template>
@@ -78,13 +121,17 @@
 import ProgressBar from "@/components/ProgressBar.vue";
 import qrcode from '@chenfengyuan/vue-qrcode';
 import { QrcodeStream } from 'vue-qrcode-reader';
+import { showAt, hideAt } from 'vue-breakpoints';
+import VueLottiePlayer from 'vue-lottie-player';
 
 export default {
     name: 'TaskInfoModalInstructorExpanded',
     components: {
         ProgressBar,
         qrcode,
-        QrcodeStream
+        QrcodeStream,
+        hideAt,
+        vLottiePlayer: VueLottiePlayer
     },
     props: {
         taskInfo: Object,
@@ -133,6 +180,95 @@ export default {
 .task-info-modal-instructor-expanded {
     border-radius: 3px;
     margin-bottom: 30px;
+    display: flex;
+
+    .left-content-area {
+        flex-grow: 1;
+    }
+
+    .attendance-area {
+        width: 40%;
+        min-width: 40%;
+        padding: 0;
+        position: relative;
+        
+        .header-area {
+            display: flex;
+            margin: 0;
+            padding: 0;
+            height: 30px;
+            line-height: 30px;
+            font-size: 0.85rem;
+            cursor: pointer;
+
+            .status-col {
+                width: 80px;
+                min-width: 80px;
+                font-weight: 600;
+                padding-left: 5px;
+                text-align: center;
+            }
+
+            .student-col {
+                flex-grow: 1;
+                font-weight: 600;
+                padding-left: 5px;
+            }
+
+            .total-col {
+                padding-right: 15px;
+            }
+        }
+
+        .attendance-entry-container {
+            overflow-y: scroll;
+            position: absolute;
+            left: 0;
+            right: 0;
+            bottom: 50px;
+            top: 30px;
+
+            .entry-area {
+                display: flex;
+                height: 27px;
+                line-height: 27px;
+
+                .status-col {
+                    width: 80px;
+                    min-width: 80px;
+                    padding-left: 5px;
+                    text-align: center;
+                }
+
+                .student-col {
+                    flex-grow: 1;
+                    padding-left: 5px;
+                }
+            }
+        }
+
+        .footer-area {
+            position: absolute;
+            left: 0;
+            right: 0;
+            bottom: 0;
+            display: flex;
+            margin: 0;
+            padding: 0;
+            height: 50px;
+            line-height: 30px;
+            font-size: 0.85rem;
+            cursor: pointer;
+
+            .spacer {
+                flex-grow: 1;
+            }
+
+            .button-area {
+                padding-right: 10px;
+            }
+        }
+    }
 
     .fullscreen-modal {
         position: fixed;
@@ -211,9 +347,23 @@ export default {
     .fullscreen-modal {
         background-color: white;
     }
+
+    .attendance-area {
+        border-left: 1px solid rgba(0, 0, 0, 0.15);
+        .header-area {
+            border-bottom: 1px solid rgba(0, 0, 0, 0.15);
+        }
+    }
 }
 
 .dark-mode {
+    .attendance-area {
+        border-left: 1px solid rgba(255, 255, 255, 0.15);
+        .header-area {
+            border-bottom: 1px solid rgba(255, 255, 255, 0.15);
+        }
+    }
+
     // 282c36
     .task-info-modal-instructor-expanded {
         background-color: #282c36;
