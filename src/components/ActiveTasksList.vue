423c09cf2734445c9e29b46d8a31aef9dd210e53
Numfor Mbiziwo-Tiapo <numsmt2@gmail.com>
2020-08-23 11:02:19 -0400

    Add ActiveTasksList component

---
+++ src/components/ActiveTasksList.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="active-tasks-list">
+
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'ActiveTasksList',
+  data () {
+    return {
+
+    }
+  },
+  async created () {
+
+  },
+  methods: {
+
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
