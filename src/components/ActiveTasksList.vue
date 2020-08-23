385e53cd0457c361cd7a90a8cd0d4f0e9eb863aa
Numfor Mbiziwo-Tiapo <numsmt2@gmail.com>
2020-08-23 14:01:07 -0400

    Link Watch Recording button in ActiveTasksList to WatchRecording view

--- src/components/ActiveTasksList.vue
+++ src/components/ActiveTasksList.vue
@@ -2,15 +2,20 @@
   <div class="active-tasks-list">
     <div class="inline-block active-task-container" v-for="task in active_tasks"
     :key="task._id">
-      <sui-button v-if="task.qr_checkin_start_time == null"
-      icon="play circle" label-position="left">
-        <a is="sui-label" slot="label" basic>Watch Recording</a>
-      </sui-button>
-      <sui-button v-else icon="qrcode" label-position="left"
-      @click="$emit('show-fullscreen-code',task.code)">
-        <a is="sui-label" slot="label" basic
-        @click="$emit('show-fullscreen-code',task.code)">Show QR Code</a>
-      </sui-button>
+      <!-- Recording Button -->
+      <router-link v-if="task.qr_checkin_start_time == null"
+      :to="{name: 'watch_recording', params: {recording_id: task._id}}"
+      @click="$emit('show-fullscreen-code',task.code)" basic>
+        <sui-button icon="play circle" label-position="left">
+          <a is="sui-label" slot="label" basic>Watch Recording</a>
+        </sui-button>
+      </router-link>
+      <!-- QR Button -->
+      <div v-else @click="$emit('show-fullscreen-code',task.code)">
+        <sui-button icon="qrcode" label-position="left">
+          <a is="sui-label" slot="label" basic>Show QR Code</a>
+        </sui-button>
+      </div>
     </div>
   </div>
 </template>
