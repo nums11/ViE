b0ee78deea3ef2ba5fe0f4a1598839852ab2d3fe
Numfor Mbiziwo-Tiapo <numsmt2@gmail.com>
2020-10-01 17:05:23 -0400

    Change to byakugan

--- src/views/MeetingInfo.vue
+++ src/views/MeetingInfo.vue
@@ -313,7 +313,7 @@ export default {
     scannedCodeIsValid(open_checkin, scanned_code) {
       let url = ""
       if(process.env.NODE_ENV === "production") {
-        url = "https://venue-attend.herokuapp.com/"
+        url = "https://byakugan.herokuapp.com/"
       } else {
         url = "http://localhost:8080/"
       }
