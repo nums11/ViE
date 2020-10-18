3c2071c5b3b8d1be7baae5ff68bb30d245e6eba0
Numfor Mbiziwo-Tiapo <numsmt2@gmail.com>
2020-10-17 20:29:13 -0400

    Route API calls back to Byakugan

--- src/services/API.js
+++ src/services/API.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 export default() => {
   return axios.create({
     baseURL: (process.env.NODE_ENV === 'production' ? 
-      `http://localhost:4000/` :
+      `https://byakugan.herokuapp.com/` :
       `http://localhost:4000/`),
     withCredentials: true
   })
