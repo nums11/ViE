43f3e00cf6bed642722d0b950d20a5d2994f3fc0
Numfor Mbiziwo-Tiapo <numsmt2@gmail.com>
2020-10-21 15:29:31 -0400

    Move notification scheduling to the server

--- server/Notification/Notification.route.js
+++ server/Notification/Notification.route.js
@@ -3,6 +3,7 @@ const notificationRoutes = express.Router();
 const webpush = require("web-push");
 const publicVapidKey = "BG5zFCphvwcm3WYs3N5d41jO85PcvpJkEYPlz9j3OjVdzI_XX0KPw_U8V5aEmaOBHXIymaGcCWyOAH-TmoobXKA"
 const privateVapidKey = "Zu01PHDOctOOBOt2QqjcwWwQg0v5xMSR7QZ-w_a7Pm0"
+const schedule = require('node-schedule');
 
 let User = require('../User/User.model');
 
@@ -38,6 +39,51 @@ const sendNotificationToUser = async (user_id, payload, route) => {
   })
   return notification_promises
 }
+
+const sendScheduledShowQRNotificationsToInstructors = async (primary_instructor_id,
+  secondary_instructor_id, meeting_id) => {
+  const redirect_url = process.env.NODE_ENV === "production" ?
+  `https://byakugan.herokuapp.com/#/meeting_info/${meeting_id}` :
+  `http://localhost:8080/#/meeting_info/${meeting_id}`
+  console.log("Meeting Info url", redirect_url)
+  const payload = JSON.stringify({
+    title: "Venue - It's time to show your QR Code!",
+    redirect_url: redirect_url
+  });
+  const route = "notifications/schedule_show_qr"
+  let notification_promises = []
+  let instructor_notification_promises = await sendNotificationToUser(primary_instructor_id, payload, route)
+  notification_promises =  notification_promises.concat(instructor_notification_promises)
+  if(secondary_instructor_id !== "null"){
+    let secondary_instructor_notification_promises =
+      await sendNotificationToUser(secondary_instructor_id, payload, route)
+    notification_promises = notification_promises.concat(secondary_instructor_notification_promises)
+  }
+
+  try {
+    const notifications = await Promise.all(notification_promises)
+    console.log(`<SUCCESS> (${route}) sending notifications to instructor(s)`)
+  } catch (error) {
+    console.log(`<ERROR> (${route}) awaiting notifications to be sent to instructor(s)`,
+      primary_instructor_id, secondary_instructor_id, error)
+  }
+}
+
+notificationRoutes.post('/schedule_show_qr/:primary_instructor_id/:secondary_instructor_id/:meeting_id',
+  (req, res) => {
+  let primary_instructor_id = req.params.primary_instructor_id
+  let secondary_instructor_id = req.params.secondary_instructor_id
+  let meeting_id = req.params.meeting_id
+  let qr_checkin_start_time = new Date(req.body.qr_checkin_start_time)
+  console.log("QR checkin start time", qr_checkin_start_time)
+
+  let job = schedule.scheduleJob(qr_checkin_start_time, function(){
+    sendScheduledShowQRNotificationsToInstructors(primary_instructor_id,
+      secondary_instructor_id, meeting_id)
+  });
+  console.log("scheduled job", job)
+  res.json({})
+})
 
 notificationRoutes.post('/show_qr/:primary_instructor_id/:secondary_instructor_id/:meeting_id',
   async (req, res) => {
