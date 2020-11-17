4fd9a45bd54143dde99f1a12ff3ec068a8e4315d
Numfor Mbiziwo-Tiapo <numsmt2@gmail.com>
2020-11-17 12:57:23 -0500

    Changes

--- server/seeds/seed_helper.js
+++ server/seeds/seed_helper.js
@@ -1,4 +1,5 @@
 const seeder = require("mongoose-seed");
+const moment = require('moment')
 const Course = require("../Course/Course.model")
 const Organization = require("../Organization/Organization.model")
 const LiveAttendance = require("../LiveAttendance/LiveAttendance.model")
@@ -15,7 +16,17 @@ const DB = require('../DB.js');
 const bcrypt = require('bcrypt');
 const saltRounds = 10;
 
-const clearAllModels = function() {
+module.exports = {
+	models: {User, Course, Meeting, LiveAttendance, AsyncAttendance,
+						Recording, QRCheckin, LiveSubmission, AsyncSubmission,
+						clearAllModels},
+	functions: {clearAllModels, getObjectIDsFromArray,
+							getPastMeetingTimes, generateRandomCode,
+							addFiveMinutes, getRandomTimeBetweenTwoTimes,
+							getRandomIDsFromArray, getRandomInt} 
+};
+
+function clearAllModels() {
 	seeder.connect(process.env.MONGODB_URI || DB.DB_URL, function () {
 		// seeder loads from the server folder
 		seeder.loadModels([
@@ -40,24 +51,42 @@ const clearAllModels = function() {
 	})
 }
 
-const getObjectIDsFromArray = function(arr, start, end) {
+function getObjectIDsFromArray(arr) {
 	let object_ids = []
-	for(let i = start; i < end; i++) {
-		object_ids.push(arr[i]._id)
-	}
+	arr.forEach(elem => {
+		object_ids.push(elem._id)
+	})
 	return object_ids
 }
 
-// Returns 2 hr long meeting times from last week's Monday
-const getPreviousMondayMeetingTimes = function() {
-	let prev_monday = new Date();
-	prev_monday.setDate(prev_monday.getDate() - (prev_monday.getDay() + 6) % 7);
-	let meeting_start = prev_monday
-	let meeting_end = new Date(meeting_start.getTime() + (2*60*60*1000))
+function getRandomIDsFromArray(arr, num_rand_ids) {
+	let rand_ids = new Set()
+	while(num_rand_ids > 0) {
+		let rand_index = getRandomInt(0, arr.length-1)
+		let rand_id = arr[rand_index]._id
+		if(!rand_ids.has(rand_id)){
+			rand_ids.add(rand_id)
+			num_rand_ids--
+		}
+	}
+	return rand_ids
+}
+
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+// Returns 2 hr long meeting times from either last Monday or last Thursday
+function getPastMeetingTimes(is_monday_meeting) {
+	let day_index = is_monday_meeting ? 1 : 4
+	let meeting_start = moment().day(day_index-7)
+	let meeting_end = moment(meeting_start).add(2, 'h')
 	return {meeting_start, meeting_end}
 }
 
-const generateRandomCode = function() {
+function generateRandomCode() {
   const alnums = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
   let result = "";
   for (let i = 100; i > 0; --i) {
@@ -66,11 +95,12 @@ const generateRandomCode = function() {
   return result;
 }
 
-module.exports = {
-	models: {User, Course, Meeting, LiveAttendance, AsyncAttendance,
-						Recording, QRCheckin, LiveSubmission, AsyncSubmission,
-						clearAllModels},
-	functions: {clearAllModels, getObjectIDsFromArray,
-							getPreviousMondayMeetingTimes, generateRandomCode} 
-};
+function addFiveMinutes(date) {
+	return moment(date).add(5,'m')
+}
+
+function getRandomTimeBetweenTwoTimes(start, end) {
+	let random_time = new Date(start.getTime() + Math.random() * (end.getTime() - start.getTime()))
+	return random_time
+}
 
--- server/seeds/small_seed.js
+++ server/seeds/small_seed.js
@@ -1,105 +1,187 @@
+/*
+Seeds database with these parameters
+- 1 Instructor
+- 16 Students
+- 1 Course
+- 3 Meetings for the course
+	- 1 with only live attendance
+	- 1 with only async attendance
+	- 1 with both live and async attendance
+*/
+
 const SeedHelper = require('./seed_helper')
 
+let instructors = [], students = [], courses = [],
+		meetings = [], live_attendances = [],
+		async_attendances = [],
+		qr_checkins = [], recordings = [],
+		live_submissions = [], async_submissions = [],
+		is_monday_meeting = true
+
 SeedHelper.functions.clearAllModels()
+createInstructor()
+createStudents()
+createCourse("RCOS", "CSCI", 2961, instructors[0],students)
+createMeeting(courses[0], true, 9, false, 0)
+createMeeting(courses[0], false, 0, true, 5)
+createMeeting(courses[0], true, 10, true, 5)
 
-// Create Users (17)
-let users = []
-
-// 1 Instructor
-users.push(new SeedHelper.models.User({ // 0
-	first_name: "Numfor",
-	last_name: "Mbiziwo-Tiapo",
-	user_id: "mbizin",
-	email: "venue@rpi.edu",
-	password: "nimda",
-	is_instructor: true,
-	is_admin: true,
-	instructor_courses: [],
-	student_courses: [],
-	users_orgs: [],
-	meetings: [],
-	live_submissions: [],
-	async_submissions: [],
-	service_worker_subscriptions: []
-}))
-
-// 16 Students
-for(let i=0;i<16;i++) { //a-p (1-17)
-	var chr = String.fromCharCode(97 + i);
-	users.push(new SeedHelper.models.User({
-		first_name: "Student",
-		last_name: chr,
-		user_id: "student" + chr,
-		email: "student"+chr+"@rpi.edu",
-		password: "password",
-		is_instructor: false,
-		is_admin: false,
+function createInstructor() {
+	instructors.push(new SeedHelper.models.User({
+		first_name: "Numfor",
+		last_name: "Mbiziwo-Tiapo",
+		user_id: "mbizin",
+		email: "venue@rpi.edu",
+		password: "nimda",
+		is_instructor: true,
+		is_admin: true,
 		instructor_courses: [],
 		student_courses: [],
 		users_orgs: [],
 		meetings: [],
 		live_submissions: [],
 		async_submissions: [],
+		service_worker_subscriptions: []
 	}))
 }
 
-// Create Courses (1)
-let courses = []
+function createStudents() {
+	for(let i=0;i<16;i++) { //a-p 
+		students.push(new SeedHelper.models.User({
+			first_name: "Student",
+			last_name: `${i + 1}`,
+			user_id: `student${i + 1}`,
+			email: `student${i + 1}@rpi.edu`,
+			password: "password",
+			is_instructor: false,
+			is_admin: false,
+			instructor_courses: [],
+			student_courses: [],
+			users_orgs: [],
+			meetings: [],
+			live_submissions: [],
+			async_submissions: [],
+		}))
+	}
+}
 
-courses.push(new SeedHelper.models.Course({ // 0
-	name: "RCOS",
-	dept: "CSCI",
-	course_number: 2961,
-	instructor: users[0]._id,
-	students: SeedHelper.functions.getObjectIDsFromArray(users, 1, 17),
-	meetings: []
-}))
+function createCourse(name, dept, course_number, instructor, students) {
+	let course = new SeedHelper.models.Course({ // 0
+		name: name,
+		dept: dept,
+		course_number: course_number,
+		instructor: instructor,
+		students: students,
+		meetings: []
+	})
+	courses.push(course)
+	instructor.instructor_courses.push(course._id)
+	students.forEach(student => student.student_courses.push(course._id))
+}
 
-// Create Meetings (4)
-let meetings = []
-let live_attendances = []
-let qr_checkins = []
-let live_submissions = []
-let async_atttendances = []
-let recordings = []
-let async_submissions = []
+function createMeeting(course, has_live_attendance, num_live_submissions,
+	has_async_attendance, num_async_submissions) {
+	let meeting_start = null, meeting_end = null
+	let only_async_meeting = has_async_attendance && !has_live_attendance
+	if(!only_async_meeting){
+		let meeting_times = SeedHelper.functions.getPastMeetingTimes(is_monday_meeting)
+		meeting_start = meeting_times.meeting_start
+		meeting_end = meeting_times.meeting_end
+		is_monday_meeting = !is_monday_meeting
+	}
 
-// 2 past meetings (1 with async)
-let {meeting_start, meeting_end} = 
-	SeedHelper.functions.getPreviousMondayMeetingTimes()
-console.log("Meeting Start", meeting_start.toString())
-console.log("Meeting End", meeting_end.toString())
+	let live_attendance = new SeedHelper.models.LiveAttendance({
+		qr_checkins: []
+	})
+	let async_attendance = new SeedHelper.models.AsyncAttendance({
+		recordings: []
+	})
+	let meeting = new SeedHelper.models.Meeting({
+		title: `Meeting ${meetings.length + 1}`,
+		start_time: meeting_start,
+		end_time: meeting_end,
+		for_course: true,
+		course: course._id,
+		has_live_attendance: has_live_attendance,
+		has_async_attendance: has_async_attendance,
+		live_attendance: live_attendance,
+		async_attendance: async_attendance
+	})
 
-meetings.push(new SeedHelper.models.Meeting({
-	title: "Meeting 1",
-	start_time: meeting_start,
-	end_time: meeting_end,
-	for_course: true,
-	course: courses[0]._id
-	has_live_attendance: true,
-	has_async_attendance: false,
-	live_attendance: null,
-	async_atttendance: null
-}))
+	if(has_live_attendance) {
+		let qr_checkin = new SeedHelper.models.QRCheckin({
+			code: SeedHelper.functions.generateRandomCode(),
+			qr_checkin_start_time: meeting_start,
+			qr_checkin_end_time: SeedHelper.functions.addFiveMinutes(meeting_start),
+			qr_checkin_submissions: []
+		})
 
-live_attendances.push(new SeedHelper.models.LiveAttendance({
-	qr_checkins: []
-}))
+		let qr_checkin_submissions = []
+		let submission_ids = Array.from(SeedHelper.functions.getRandomIDsFromArray(
+			course.students, num_live_submissions))
+		for(let i = 0; i < num_live_submissions; i++) {
+			qr_checkin_submissions.push(new SeedHelper.models.LiveSubmission({
+				submitter: submission_ids[i],
+				live_submission_time: SeedHelper.functions.getRandomTimeBetweenTwoTimes(
+															qr_checkin.qr_checkin_start_time,
+															qr_checkin.qr_checkin_end_time),
+				is_qr_checkin_submission: true,
+				qr_checkin: qr_checkin._id,
+			}))
+		}
+		qr_checkin.qr_checkin_submissions = qr_checkin_submissions
 
-qr_checkins.push(new SeedHelper.models.QRCheckin({
-	code: SeedHelper.functions.generateRandomCode(),
-	qr_checkin_start_time: null,
-	qr_checkin_end_time: null,
-	qr_checkins_submissions: []
-}))
+		qr_checkins.push(qr_checkin)
+		live_submissions = live_submissions.concat(qr_checkin_submissions)
+	}
 
-// 1 ongoing meeting (live)
+	if(has_async_attendance) {
+		let recording_submission_start = null,
+				recording_submission_end = null
+		// Use the meeting times if possible otherwise generate times for the recording
+		if(only_async_meeting) {
+			let meeting_times = SeedHelper.functions.getPastMeetingTimes(is_monday_meeting)
+			recording_submission_start = meeting_times.meeting_start
+			recording_submission_end = meeting_times.meeting_end
+			is_monday_meeting = !is_monday_meeting
+		} else {
+			recording_submission_start = meeting_start
+			recording_submission_end = meeting_end
+		}
+		let recording = new SeedHelper.models.Recording({
+			video_url: "https://storage.googleapis.com/venue_videos/sample1.mp4",
+			recording_submission_start_time: recording_submission_start,
+			recording_submission_end_time: recording_submission_end,
+			recording_submissions: []
+		})
 
-// meetings.push(new Meeting({
+		let recording_submissions = []
+		let submission_ids = Array.from(SeedHelper.functions.getRandomIDsFromArray(
+			course.students, num_async_submissions))
+		for(let i = 0; i < num_async_submissions; i++) {
+			// Length of sample1.mp4 is 13 seconds
+			let video_length = 13
+			let furthest_video_time = SeedHelper.functions.getRandomInt(0,video_length)
+			recording_submissions.push(new SeedHelper.models.AsyncSubmission({
+				submitter: submission_ids[i],
+				is_recording: true,
+				recording: recording._id,
+				furthest_video_time: furthest_video_time,
+				video_percent_watched: furthest_video_time/13
+			}))
+		}
+		recording.recording_submissions = recording_submissions
 
-// }))
+		recordings.push(recording)
+		async_submissions = async_submissions.concat(recording_submissions)
+	}
 
-// Create Submissions for those meetings
+	// Update the course, instructor, and students
+	course.meetings.push(meeting)
+	course.instructor.meetings.push(meeting)
+	course.students.forEach(student => student.meetings.push(meeting))
 
-
-// console.log(c)
+	live_attendances.push(meeting.live_attendance)
+	async_attendances.push(meeting.async_attendance)
+	meetings.push(meeting)
+}
