1d70cfa3fc41575239cceacace1fce652022e44d
Numfor Mbiziwo-Tiapo <numsmt2@gmail.com>
2020-11-25 13:34:39 -0500

    Delete old specs

--- cypress/integration/cas_login_spec.js
+++
@@ -1,8 +0,0 @@
-// describe('CAS Login Test', () => {
-// 	it('Ensures that CAS login works', () => {
-//     cy.visit('http://localhost:8080')
-//     cy.contains('Login').click()
-//     cy.get('#username').type('mbizin')
-//     cy.get('#password').type('Numfor#135')
-// 	})
-// })
--- run_integration_tests.sh
+++ run_integration_tests.sh
@@ -3,23 +3,22 @@
 seed_sizes=("small")
 
 # Local testing
-# for seed_size in "${seed_sizes[@]}"
-# do
-# 	echo "---------------- Running test locally with $seed_size seed_size ----------------"
-# 	npx cypress run --spec "cypress/integration/Actions/meeting_creation_spec.js"\
-# 	 -b chrome --headless --record --key 2be23156-7680-4fd8-bce1-e0c09f23c002\
-# 	 -e seed_size="$seed_size"
-# done
-
-# Production Testing
 for seed_size in "${seed_sizes[@]}"
 do
-	echo "---------------- Running test in production with $seed_size seed_size ----------------"
-	CYPRESS_BASE_URL=https://byakugan.herokuapp.com/# npx cypress run\
-	 --spec "cypress/integration/Actions/meeting_creation_spec.js"\
-	 -b chrome --headless --record --key 2be23156-7680-4fd8-bce1-e0c09f23c002\
-	 -e seed_size="$seed_size",prod_mode=true
+	echo "---------------- Running test locally with $seed_size seed_size ----------------"
+	npx cypress run -b chrome --headless --record --key 2be23156-7680-4fd8-bce1-e0c09f23c002\
+	 -e seed_size="$seed_size"
 done
+
+# Production Testing
+# for seed_size in "${seed_sizes[@]}"
+# do
+# 	echo "---------------- Running test in production with $seed_size seed_size ----------------"
+# 	CYPRESS_BASE_URL=https://byakugan.herokuapp.com/# npx cypress run\
+# 	 --spec "cypress/integration/Actions/meeting_creation_spec.js"\
+# 	 -b chrome --headless --record --key 2be23156-7680-4fd8-bce1-e0c09f23c002\
+# 	 -e seed_size="$seed_size",prod_mode=true
+# done
 
 npx cypress open
 
