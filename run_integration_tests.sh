1e0ddd489fdd4366cd26541e2db7033c7cc4cbac
Numfor Mbiziwo-Tiapo <numsmt2@gmail.com>
2020-11-25 17:35:42 -0500

    Add browser variance

--- run_integration_tests.sh
+++ run_integration_tests.sh
@@ -1,34 +1,30 @@
 #!/bin/bash
 
-seed_sizes=("small")
+seed_sizes=("small" "medium" "large")
+browsers=("chrome" "firefox" "/usr/bin/microsoft-edge")
 
 # Local testing
-for seed_size in "${seed_sizes[@]}"
+for browser in "${browsers[@]}"
 do
-	echo "---------------- Running test locally with $seed_size seed_size ----------------"
-	npx cypress run -b chrome --headless --record --key 2be23156-7680-4fd8-bce1-e0c09f23c002\
-	 -e seed_size="$seed_size"
+	for seed_size in "${seed_sizes[@]}"
+	do
+		echo "---------------- Running Tests - ENV: Local, Browser: $browser, Seed Size: $seed_size  ----------------"
+		npx cypress run -b "$browser" --headless --record --key 2be23156-7680-4fd8-bce1-e0c09f23c002\
+		 -e seed_size="$seed_size"
+	done
 done
 
 # Production Testing
-# for seed_size in "${seed_sizes[@]}"
-# do
-# 	echo "---------------- Running test in production with $seed_size seed_size ----------------"
-# 	CYPRESS_BASE_URL=https://byakugan.herokuapp.com/# npx cypress run\
-# 	 --spec "cypress/integration/Actions/meeting_creation_spec.js"\
-# 	 -b chrome --headless --record --key 2be23156-7680-4fd8-bce1-e0c09f23c002\
-# 	 -e seed_size="$seed_size",prod_mode=true
-# done
+for browser in "${browsers[@]}"
+do
+	for seed_size in "${seed_sizes[@]}"
+	do
+		echo "---------------- Running Tests - ENV: Production, Browser: $browser, Seed Size: $seed_size  ----------------"
+		CYPRESS_BASE_URL=https://byakugan.herokuapp.com/# npx cypress run\
+		 -b "$browser" --headless --record --key 2be23156-7680-4fd8-bce1-e0c09f23c002\
+		 -e seed_size="$seed_size",prod_mode=true
+	done
+done
+
 
 npx cypress open
-
-# Running all tests in production with different seed sizes
-# CYPRESS_BASE_URL=https://byakugan.herokuapp.com/# npx cypress run\
-#  -e seed_size=small,prod_mode=true
-# CYPRESS_BASE_URL=https://byakugan.herokuapp.com/# npx cypress run\
-#  -e seed_size=medium,prod_mode=true
-# CYPRESS_BASE_URL=https://byakugan.herokuapp.com/# npx cypress run\
-#  -e seed_size=large,prod_mode=true
-
-# Varying browser locally and in prod
-# Simply just changing the --browser flag
