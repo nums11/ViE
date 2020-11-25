26a12a0b7092bc0b03209e8eb9d41f2d2d0c5165
Numfor Mbiziwo-Tiapo <numsmt2@gmail.com>
2020-11-24 21:29:10 -0500

    Uncomment tests

--- cypress/integration/Actions/meeting_creation_spec.js
+++ cypress/integration/Actions/meeting_creation_spec.js
@@ -31,10 +31,6 @@ describe('Meeting Creation', () => {
     cy.contains('Schedule Meeting').click()
   })
 
-  // it('does nothing', () => {
-  //   cy.task("log","In test")
-  // })
-
   // Still need to add test for uploading large video file
 
   it('Can create a meeting with a live task', () => {
@@ -60,74 +56,75 @@ describe('Meeting Creation', () => {
       contains('Meeting with live task')
   })
 
-  // it('Can create a meeting with an async task',() => {
-  //   cy.get('#meeting-title').type('Meeting with async task')
-  //     .should('have.value', 'Meeting with async task')
-  //   cy.get('#recording-checkbox').click()
-  //   cy.get('#recording_submission_start').click()
-  //   cy.get('.left-side-area').click()
-  //   cy.fixture('sample1.mp4').then(file_content => {
-  //     cy.get('#recording-upload-input').attachFile({
-  //         fileContent: file_content.toString(),
-  //         fileName: 'sample1.mp4',
-  //         mimeType: 'video/mp4'
-  //     })
-  //   })
-  //   cy.contains('Create New Meeting').click()
-  //   cy.url({timeout: 5000}).should('contain', 'meeting_info')
-  //   cy.get('#meeting-title-header').should('contain','Meeting with async task')
-  //   cy.get('#course-name-label').should('contain','RCOS')
-  //   cy.get('#course-dept-label').should('contain', 'CSCI 2961')
-  //   cy.contains('(1) Asynchronous Tasks')
-  //   cy.get('.task-info-container').should('contain', 'Recording')
-  //   cy.contains('Dashboard').click({force: true})
-  //   cy.get('#async-section .meeting-info-pill').should('contain','Meeting with async task')
-  //     .and('contain', 'CSCI 2961').and('contain', 'RCOS')
-  //   cy.contains('RCOS').click({force: true})
-  //   cy.get('.meeting-attendance-pill').contains('Meeting with async task')
-  // })
+  it('Can create a meeting with an async task',() => {
+    cy.get('#meeting-title').type('Meeting with async task')
+      .should('have.value', 'Meeting with async task')
+    cy.get('#recording-checkbox').click()
+    cy.get('#recording_submission_start').click()
+    cy.get('.left-side-area').click()
+    cy.fixture('sample1.mp4').then(file_content => {
+      cy.get('#recording-upload-input').attachFile({
+          fileContent: file_content.toString(),
+          fileName: 'sample1.mp4',
+          mimeType: 'video/mp4'
+      })
+    })
+    cy.contains('Create New Meeting').click()
+    cy.url({timeout: 5000}).should('contain', 'meeting_info')
+    cy.get('#meeting-title-header').should('contain','Meeting with async task')
+    cy.get('#course-name-label').should('contain','RCOS')
+    cy.get('#course-dept-label').should('contain', 'CSCI 2961')
+    cy.contains('(1) Asynchronous Tasks')
+    cy.get('.task-info-container').should('contain', 'Recording')
+    cy.contains('Dashboard').click({force: true})
+    cy.get('#async-section .meeting-info-pill').should('contain','Meeting with async task')
+      .and('contain', 'CSCI 2961').and('contain', 'RCOS')
+    cy.contains('RCOS').click({force: true})
+    cy.get('.meeting-attendance-pill', {timeout: 10000}).
+      contains('Meeting with async task')
+  })
 
-  // it('Can create a meeting with both a live task and an async task',
-  //   () => {
-  //     cy.get('#meeting-title').type('Meeting with live and async task')
-  //       .should('have.value', 'Meeting with live and async task')
-  //     cy.get('#qr-checkbox').click()
-  //     cy.get('#meeting_start').click()
-  //     cy.get('.left-side-area').click()
-  //     cy.get('#qr_checkin_start').click()
-  //     cy.get('.left-side-area').click()
-  //     cy.get('#recording-checkbox').click()
-  //     cy.get('#recording_submission_start').click()
-  //     cy.get('.left-side-area').click()
-  //     cy.fixture('sample1.mp4').then(file_content => {
-  //       cy.get('#recording-upload-input').attachFile({
-  //           fileContent: file_content.toString(),
-  //           fileName: 'sample1.mp4',
-  //           mimeType: 'video/mp4'
-  //       })
-  //     })
-  //     cy.contains('Create New Meeting').click()
-  //     cy.url({timeout: 5000}).should('contain', 'meeting_info')
-  //     cy.get('#meeting-title-header')
-  //       .should('contain','Meeting with live and async task')
-  //     cy.get('#course-name-label').should('contain','RCOS')
-  //     cy.get('#course-dept-label').should('contain', 'CSCI 2961')
-  //     cy.contains('(1) Live Tasks')
-  //     cy.contains('(1) Asynchronous Tasks')
-  //     cy.get('.task-info-container').should('contain', 'QR Submission')
-  //       .and('contain', 'Recording')
-  //     cy.contains('Dashboard').click({force: true})
-  //     cy.get('#live-section .meeting-info-pill')
-  //       .should('contain','Meeting with live and async task')
-  //       .and('contain', 'CSCI 2961').and('contain', 'RCOS')
-  //     cy.get('#async-section .meeting-info-pill')
-  //       .should('contain','Meeting with live and async task')
-  //       .and('contain', 'CSCI 2961').and('contain', 'RCOS')
-  //     cy.contains('RCOS').click({force: true})
-  //     cy.get('.meeting-attendance-pill')
-  //       .contains('Meeting with live and async task')
-  //   }
-  // )
+  it('Can create a meeting with both a live task and an async task',
+    () => {
+      cy.get('#meeting-title').type('Meeting with live and async task')
+        .should('have.value', 'Meeting with live and async task')
+      cy.get('#qr-checkbox').click()
+      cy.get('#meeting_start').click()
+      cy.get('.left-side-area').click()
+      cy.get('#qr_checkin_start').click()
+      cy.get('.left-side-area').click()
+      cy.get('#recording-checkbox').click()
+      cy.get('#recording_submission_start').click()
+      cy.get('.left-side-area').click()
+      cy.fixture('sample1.mp4').then(file_content => {
+        cy.get('#recording-upload-input').attachFile({
+            fileContent: file_content.toString(),
+            fileName: 'sample1.mp4',
+            mimeType: 'video/mp4'
+        })
+      })
+      cy.contains('Create New Meeting').click()
+      cy.url({timeout: 5000}).should('contain', 'meeting_info')
+      cy.get('#meeting-title-header')
+        .should('contain','Meeting with live and async task')
+      cy.get('#course-name-label').should('contain','RCOS')
+      cy.get('#course-dept-label').should('contain', 'CSCI 2961')
+      cy.contains('(1) Live Tasks')
+      cy.contains('(1) Asynchronous Tasks')
+      cy.get('.task-info-container').should('contain', 'QR Submission')
+        .and('contain', 'Recording')
+      cy.contains('Dashboard').click({force: true})
+      cy.get('#live-section .meeting-info-pill')
+        .should('contain','Meeting with live and async task')
+        .and('contain', 'CSCI 2961').and('contain', 'RCOS')
+      cy.get('#async-section .meeting-info-pill')
+        .should('contain','Meeting with live and async task')
+        .and('contain', 'CSCI 2961').and('contain', 'RCOS')
+      cy.contains('RCOS').click({force: true})
+      cy.get('.meeting-attendance-pill', {timeout: 10000})
+        .contains('Meeting with live and async task')
+    }
+  )
 
 })
 
