68fada7c5fe5ad77a108dd375de2da5203efa614
Numfor Mbiziwo-Tiapo <numsmt2@gmail.com>
2020-11-25 13:29:13 -0500

    Loop through seed sizes in test script

--- run_integration_tests.sh
+++ run_integration_tests.sh
@@ -1,21 +1,27 @@
-echo "---------------- Running test with small seed_size ----------------"
-npx cypress run --spec "cypress/integration/Actions/meeting_creation_spec.js"\
- -b chrome --headless --record --key 2be23156-7680-4fd8-bce1-e0c09f23c002\
- --group "small_seeds2" --ci-build-id local2 -e seed_size=small
+#!/bin/bash
 
-echo "---------------- Running test with medium seed_size ----------------"
-npx cypress run --spec "cypress/integration/Actions/meeting_creation_spec.js"\
- -b chrome --headless --record --key 2be23156-7680-4fd8-bce1-e0c09f23c002\
- --group "small_seeds2" --ci-build-id local2 -e seed_size=medium
+seed_sizes=("small")
 
-# Running all tests locally with different seed sizes
-# Could probably convert into a for loop
-# npx cypress run -e seed_size=small\
-#  --record --key 2be23156-7680-4fd8-bce1-e0c09f23c002\
-# npx cypress run -e seed_size=medium\
-#  --record --key 2be23156-7680-4fd8-bce1-e0c09f23c002\
-# npx cypress run -e seed_size=large\
-#  --record --key 2be23156-7680-4fd8-bce1-e0c09f23c002\
+# Local testing
+# for seed_size in "${seed_sizes[@]}"
+# do
+# 	echo "---------------- Running test locally with $seed_size seed_size ----------------"
+# 	npx cypress run --spec "cypress/integration/Actions/meeting_creation_spec.js"\
+# 	 -b chrome --headless --record --key 2be23156-7680-4fd8-bce1-e0c09f23c002\
+# 	 -e seed_size="$seed_size"
+# done
+
+# Production Testing
+for seed_size in "${seed_sizes[@]}"
+do
+	echo "---------------- Running test in production with $seed_size seed_size ----------------"
+	CYPRESS_BASE_URL=https://byakugan.herokuapp.com/# npx cypress run\
+	 --spec "cypress/integration/Actions/meeting_creation_spec.js"\
+	 -b chrome --headless --record --key 2be23156-7680-4fd8-bce1-e0c09f23c002\
+	 -e seed_size="$seed_size",prod_mode=true
+done
+
+npx cypress open
 
 # Running all tests in production with different seed sizes
 # CYPRESS_BASE_URL=https://byakugan.herokuapp.com/# npx cypress run\
