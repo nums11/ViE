e2e915918ec6e985d9a4765a9bf2c20100a4067f
Numfor Mbiziwo-Tiapo <numsmt2@gmail.com>
2021-02-10 16:58:59 -0500

    Update the quiz when switching between questions

--- src/components/BarChart.vue
+++ src/components/BarChart.vue
@@ -10,6 +10,20 @@ export default {
   mounted () {
     this.renderChart(this.chart_data,
     	this.chart_options)
+    // console.log("chart", this.$data._chart)
   }
+  // watch: {
+  // 	chart_data: {
+	 //  	 handler(old_chart_data, new_chart_data) {
+	 //  	 	// this.chart_data = new_chart_data
+	 //  		console.log("chart_data updated", new_chart_data)
+	 //  		this.$data._chart.update()
+	 //  		// this._chart.destroy();
+	 //  		// this.renderChart(new_chart_data,
+	 //  		// 	this.chart_options)
+	 //  	},
+	 //  	deep: true
+	 //  }
+  // }
 }
 </script>
--- src/views/RealTimeQuiz.vue
+++ src/views/RealTimeQuiz.vue
@@ -1,40 +1,49 @@
 <template>
   <div>
-    <div id="btn-area">
-      <sui-button content="Previous Question"
-      icon="arrow left"
-      label-position="left"
-      style="background-color:#00B3FF; color:white;" />
-      <sui-button content="Next Question"
-      icon="arrow right"
-      label-position="right"
-      class="float-right"
-      style="background-color:#00b80c; color: white;" />
-    </div>
-    <div id="logo-container">
-      <img src="@/assets/logo.svg" id="logo" />
-    </div>
-    <div class="center-text" id="question">
-      Which astronmer was the first to complete
-      this random thing that I didn't know existed
-      and now I'm just going to make this longer for no
-      reason?
-    </div>
-    <div class="center-text wrap-text" id="submissions">
-      Submissions Received
-      <p id="num-submissions">10/20 (50%)</p>
-    </div>
-    <BarChart :chart_data="chart_data"
-    :chart_options="chart_options"
-    :style="chart_styles" />
-    <div class="mt-1 float-right" id="stop-quiz-btn">
-      <Button text="Stop Quiz" color="pink" size="small"
-      wide />
+    <sui-loader v-if="!quiz_has_loaded"
+    active size="large">
+      Loading Quiz
+    </sui-loader>
+    <div v-else>
+      <div id="btn-area">
+        <sui-button @click="changeQuestion(false)"
+        content="Previous Question"
+        icon="arrow left"
+        label-position="left"
+        style="background-color:#00B3FF; color:white;"
+        :disabled="current_question_index === 0" />
+        <sui-button @click="changeQuestion(true)"
+        content="Next Question"
+        icon="arrow right"
+        label-position="right"
+        class="float-right"
+        style="background-color:#00b80c; color: white;"
+        :disabled="current_question_index === quiz.questions.length-1" />
+      </div>
+      <div id="logo-container">
+        <img src="@/assets/logo.svg" id="logo" />
+      </div>
+      <div class="center-text" id="question">
+        {{ current_question.question }}
+      </div>
+      <div class="center-text wrap-text" id="submissions">
+        Submissions Received
+        <p id="num-submissions">10/20 (50%)</p>
+      </div>
+      <BarChart ref="BarChart"
+      :chart_data="chart_data"
+      :chart_options="chart_options"
+      :style="chart_styles" />
+      <div class="mt-1 float-right" id="stop-quiz-btn">
+        <Button text="Stop Quiz" color="pink" size="small"
+        wide />
+      </div>
     </div>
   </div>
 </template>
 
 <script>
+import QuizAPI from '@/services/QuizAPI'
 import BarChart from '@/components/BarChart'
 import Button from '@/components/Button'
 import 'chartjs-plugin-datalabels'
@@ -48,10 +57,12 @@ export default {
   data(){
     return {
       quiz_id: "",
+      quiz: null,
+      quiz_has_loaded: false,
+      current_question: null,
+      current_question_index: 0,
       chart_data: {
-        label: "hello",
-        labels: ['Option 1', 'Option 2', 'Option 3',
-          'Option 4', 'Option 5', 'Option 6'],
+        labels: [],
         datasets: [{
           backgroundColor: ['rgba(232, 62, 140, 0.65)',
           'rgba(0, 179, 255, 0.65)', 'rgba(0, 184, 12, 0.65)',
@@ -99,9 +110,36 @@ export default {
   },
   created() {
     this.quiz_id = this.$route.params.quiz_id
+    this.getQuiz()
   },
   methods: {
-
+    async getQuiz() {
+      try {
+        const response = await QuizAPI.getQuiz(this.quiz_id)
+        this.quiz = response.data
+        this.showQuestion()
+        this.quiz_has_loaded = true
+      } catch(error) {
+        console.log(error)
+        alert("Sorry, something went wrong")
+      }
+    },
+    showQuestion() {
+      this.current_question = this.quiz.questions[
+        this.current_question_index]
+      this.chart_data.labels = []
+      this.current_question.answer_choices.forEach(choice => {
+        this.chart_data.labels.push(choice)
+      })
+    },
+    changeQuestion(is_next) {
+      if(is_next)
+        this.current_question_index++
+      else
+        this.current_question_index--
+      this.showQuestion()
+      this.$refs.BarChart.$data._chart.update()
+    }
   }
 }
 </script>
@@ -115,7 +153,6 @@ export default {
 }
 
 #logo-container {
-  /*border: red solid;*/
   width: 4rem;
   /*display: inline-block;*/
   margin: auto;
